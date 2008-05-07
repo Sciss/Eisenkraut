c16d2770c1d2aeee18cdf75c42244ab3cf7ca4b1
sciss <sciss@72d441a5-a63e-0410-b92d-a332b6c80bf9>
2008-05-07 19:10:09 +0000

    adding .bat for windows (i hope it works ; well you will need to edit the "cd"). getting rid of synthdefs folder
    git-svn-id: file:///tmp/Eisenkraut@86 72d441a5-a63e-0410-b92d-a332b6c80bf9

---
+++ trunk/Eisenkraut.bat
@@ -0,0 +1,2 @@
+REM cd <whereYouInstalledEisenkraut>
+java -ea -cp "build\Eisenkraut.jar:libraries\JCollider.jar:libraries\MRJAdapter.jar:libraries\NetUtil.jar:libraries\Normalizer.jar:libraries\SwingOSC.jar" de.sciss.eisenkraut.Main
